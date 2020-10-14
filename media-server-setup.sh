ef0d2feae22f6fbc34d73bec809fbc5836468190
RetroTrigger <123jerry86@gmail.com>
2020-10-14 19:46:07 +1000

    error fix after first run

--- media-server-setup.sh
+++ media-server-setup.sh
@@ -4,6 +4,10 @@
 wget https://nzbget.net/download/nzbget-latest-bin-linux.run
 sh nzbget-latest-bin-linux.run
 
+sudo mv nzbget.service /etc/systemd/system/nzbget.service
+sudo systemctl enable --now nzbget.service
+sudo systemctl start nzbget.service
+
 ## Install Sonarr
 
 #Add Mono Repository
@@ -13,7 +17,7 @@ echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sud
 sudo apt update
 
 #Add Mediainfo Repository
-wget https://mediaarea.net/repo/deb/repo-mediaarea_1.0-13_all.deb && dpkg -i repo-mediaarea_1.0-13_all.deb && apt-get update
+wget https://mediaarea.net/repo/deb/repo-mediaarea_1.0-13_all.deb && sudo dpkg -i repo-mediaarea_1.0-13_all.deb && apt-get update
 
 #Add The Sonarr Repository
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8
@@ -25,6 +29,9 @@ sudo apt install -y sonarr
 ## Install Radarr
 
 sudo apt install -y mono-devel curl mediainfo
+cd ~
+mkdir Radarr
+cd Radarr
 curl -L -O $( curl -s https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )
 tar -xvzf Radarr.develop.*.linux.tar.gz
 
@@ -32,25 +39,22 @@ sudo mv Radarr.service /etc/systemd/system/radarr.service
 sudo systemctl enable --now radarr.service
 sudo systemctl start radarr.service
 
-## Install NZBHydra2
+## Install NZBHydra
+sudo apt-get update
+sudo apt-get install python2.7 git-core -y
+sudo git clone https://github.com/theotherp/nzbhydra /opt/nzbhydra
+sudo chown -R user:user /opt/nzbhydra
 
-sudo apt-get install -y openjdk-8-jre
-cd ~
-mkdir nzbhydra2
-cd nzbhydra2
-wget https://github.com/theotherp/nzbhydra2/releases/latest -O my-nzbhydra2.zip
-unzip my-nzbhydra2.zip 
-wget https://raw.githubusercontent.com/theotherp/nzbhydra2/master/other/wrapper/nzbhydra2wrapper.py
-python nzbhydra2wrapper.py
-
-sudo mv nzbhydra2.service /etc/systemd/system
-sudo systemctl enable nzbhydra2.service
-sudo systemctl start nzbhydra2.service
+cd ~/media-server-setup
+sudo mv nzbhydra.service
+sudo systemctl enable nzbhydra.service
+sudo systemctl start nzbhydra.service
 
 ## Install Plex Media Server
 
+cd ~
 wget https://downloads.plex.tv/plex-media-server-new/1.19.3.2852-219a9974e/debian/plexmediaserver_1.19.3.2852-219a9974e_amd64.deb
-sudo dpkg –i plexmediaserver_1.19.3..2852-219a9974e_amd64.deb
+sudo dpkg -i plexmediaserver_1.19.3.2852-219a9974e_amd64.deb
 
 sudo systemctl enable plexmediaserver.service
 sudo systemctl start plexmediaserver.service
